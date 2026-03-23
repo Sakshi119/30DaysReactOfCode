831c147d7f60be29d3a3e00b9684348d74f2bc1a
Sakshi Sen <sakshi_sen@ildc-inlp.idealake.com>
2026-03-23 16:17:51 +0530

    added the input field function and updated the increment and reset functions accordingly

--- src/components/CounterApp/Counter.jsx
+++ src/components/CounterApp/Counter.jsx
@@ -3,29 +3,30 @@ import '../../App.css'
 const Counter = () => {
 
   const [counterValue, setCounterValue] = useState(0);
-  console.log(counterValue)
+  const [inputValue, setInputValue] = useState(0)
 
   function changeInputHandle(e) {
- 
+    setInputValue(parseInt(e.target.value));
   }
 
   function increment() {
-    setCounterValue(counterValue + 1)
+    if (inputValue > 0) {
+      setCounterValue(parseInt(counterValue + inputValue))
+    } else {
+      setCounterValue(parseInt(counterValue + 1))
+    }
   }
-
 
   function decrement() {
     if (counterValue === 0) return;
-
     setCounterValue(counterValue - 1)
   }
 
 
   function reset() {
     setCounterValue(0)
+    setInputValue(0)
   }
-
-
 
   return (
     <div className="counterContainer">
@@ -33,7 +34,7 @@ const Counter = () => {
 
       <p> Counter : <span className={counterValue > 0 ? "valueColor" : "disabled"}>{counterValue}</span> </p>
       <div className="btnContainer">
-        <input type='number' value={counterValue} onChange={changeInputHandle} />
+        <input type='number' value={inputValue} onChange={changeInputHandle} />
         <button onClick={() => (increment())}>Increment</button>
         <button className={counterValue === 0 ? "disabled" : ''} onClick={() => (decrement())}>Decrement</button>
         <button onClick={() => reset()}>Reset</button>
