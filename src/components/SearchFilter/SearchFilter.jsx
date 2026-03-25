346cabd284545e69241ab5e5953a581288ed357a
Sakshi Sen <sakshi_sen@ildc-inlp.idealake.com>
2026-03-25 15:58:25 +0530

    worked on the role + name filter

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -3,23 +3,50 @@ import { data } from './data'
 const SearchFilter = () => {
     console.log(data, "data")
     const [searchval, setSearchVal] = useState("")
+    const [selectedRole, setSelectedRole] = useState("")
 
+    const roleData = [...new Set(data.map((datas) => datas.role))];
+
+    console.log(roleData,"selectedRole")
     function handleSearchFilter(e) {
         setSearchVal(e.target.value)
     }
 
-    const filteredSearchItems = data.filter(datas =>
-        datas.name.toLowerCase().includes(searchval.toLowerCase()) ||
-        datas.role.toLowerCase().includes(searchval.toLowerCase())
+    function handleDropdownFilter(e) {
+        setSelectedRole(e.target.value)
+    }
+
+    const filteredSearchItems = data.filter(datas => {
+        const matchesSearch =
+            datas.name.toLowerCase().includes(searchval.toLowerCase()) ||
+            datas.role.toLowerCase().includes(searchval.toLowerCase());
+
+        const matchesRole = selectedRole === "" || datas.role === selectedRole;
+
+        return matchesSearch && matchesRole;
+    }
     )
-    console.log(searchval)
+
     return (
         <div className="Container">
             <h2>Search Filter App</h2>
 
             <div className='search-input'>
-                <label htmlfor="searchInput" hidden>Search</label>
-                <input type='search' id='searchInput' value={searchval} onChange={handleSearchFilter} placeholder='Filter the users'></input>
+                <div className='searchbar'>
+                    <label htmlFor="searchInput" hidden>Search</label>
+                    <input type='search' id='searchInput' value={searchval} onChange={handleSearchFilter} placeholder='Filter the users'></input>
+                </div>
+                <div className='roleFilter'>
+                    <label htmlFor='roleInput' hidden>Role Filter</label>
+                    <select className='select' id='roleInput' value={selectedRole} onChange={handleDropdownFilter}>
+                        <option value=''>All</option>
+                        {roleData?.map((role, index) => {
+                            return (
+                                <option key={index} value={role}>{role}</option>
+                            )
+                        })}
+                    </select>
+                </div>
             </div>
             <p>Showing {filteredSearchItems.length} of {data.length} users</p>
             <ul className='user-list'>
@@ -28,7 +55,6 @@ const SearchFilter = () => {
                         <li key={datas.id} className='userNamesCard'>{datas.name} <span>Role:{datas.role}</span></li>
                     )
                 })}
-
                 {filteredSearchItems.length === 0 && <p>"No users found"</p>}
             </ul>
 
