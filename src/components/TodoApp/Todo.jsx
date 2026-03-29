138cfa33fccc725f6e972d0ff441bc528b746a1f
Sakshi <sensakshi5@gmail.com>
2026-03-29 22:37:26 +0530

    todo list : added the list and deleted it , added the complete : toggle

--- src/components/TodoApp/Todo.jsx
+++ src/components/TodoApp/Todo.jsx
@@ -3,14 +3,25 @@ import React, { useState } from 'react'
 const Todo = () => {
     const [task, setTask] = useState("")
     const [data, setData] = useState([]);
+    const [isCompleted, setIsCompleted] = useState(false);
     const handleTaskInput = (e) => {
         setTask(e.target.value)
     }
     function handleSubmit() {
         console.log("hi data added", task);
-        setData(prevTask => [...prevTask, task]);
+        if (task.trim() === "") return;
+        setData(prevTask => [...prevTask, { id: Date.now(), text: task, completed: isCompleted }]);
         setTask("")
     }
+    // console.log(data, "data")
+
+    function handleDelete(index) {
+        const copyOfData = [...data]
+        copyOfData.splice(index, 1);
+        setData(copyOfData)
+    }
+
+
     console.log(data, "data")
     return (
         <div className="Container">
@@ -21,15 +32,22 @@ const Todo = () => {
                 <input type="text" placeholder='Add your task' value={task} onChange={handleTaskInput} />
                 <button className='addBtn' onClick={handleSubmit}>+</button>
             </div>
-            <div className='TodoDataList'>
-                <ul>
-                    {data?.map((listData, index) => {
-                        return (
-                            <li key={index}>{listData}</li>
-                        )
-                    })}
-                </ul>
-            </div>
+            {data.length > 0 &&
+                <div className='TodoDataList'>
+                    <ul>
+                        {data?.map((listData, index) => {
+                            return (
+                                <li key={index} className={isCompleted === index ? "completed" : ""}>
+                                    <p>{listData.id}</p>
+                                    <p>{listData.text}</p>
+                                    <button onClick={() => handleDelete(index)}>Delete</button>
+                                    <button onClick={() => setIsCompleted(index, !isCompleted)}>Complete</button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
